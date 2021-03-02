8386447dd028d330a24a330b5d4dbc0c385ad7c9
Muhammed Ali Zengin <alizengin99@gmail.com>
2021-03-02 22:43:42 +0100

    Creating Group Repo!

---
+++ src/test/java/com/crm/pages/LoginPage.java
@@ -0,0 +1,28 @@
+package com.crm.pages;
+
+import com.crm.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    public LoginPage(){
+        PageFactory.initElements(Driver.get(),this);
+    }
+
+    @FindBy(name = "USER_LOGIN")
+    public WebElement userName;
+
+    @FindBy(name = "USER_PASSWORD")
+    public WebElement password;
+
+    @FindBy(className = "login-btn")
+    public WebElement logIn;
+
+    public void login(String userNameStr, String passwordStr){
+        userName.sendKeys(userNameStr);
+        password.sendKeys(passwordStr);
+        logIn.click();
+    }
+}
