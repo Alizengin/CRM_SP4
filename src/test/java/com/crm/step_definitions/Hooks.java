5032369e1ccd6778b802e67d35938f9835b4eb96
davidJacobsCybertek <71379993+davidJacobsCybertek@users.noreply.github.com>
2021-03-08 23:51:51 +0300

    David Sprint 4 added

--- src/test/java/com/crm/step_definitions/Hooks.java
+++ src/test/java/com/crm/step_definitions/Hooks.java
@@ -1,17 +1,32 @@
 package com.crm.step_definitions;
 
+import com.crm.utilities.ConfigurationReader;
 import com.crm.utilities.Driver;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
 
 public class Hooks {
+    protected WebDriver driver;
+    protected Actions actions;
+    protected WebDriverWait wait;
     @Before
     public void setUp(){
-        System.out.println("\tthis is coming from BEFORE");
-    }
+            driver = Driver.get();
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+            actions = new Actions(driver);
+            wait = new WebDriverWait(driver,10);
+            driver.get(ConfigurationReader.get("url"));
+        }
+
 
     @After
     public void tearDown(Scenario scenario){
--- src/test/java/com/crm/step_definitions/TaskMenu_StepDef.java
+++
@@ -1,7 +0,0 @@
-package com.crm.step_definitions;
-
-public class TaskMenu_StepDef {
-
-    //king of the groups is group24
-
-}
